b4e0d0c6a5c8076c5ebfb15eadaaa06edf93a22e
hvckless <hvckless@gmail.com>
2024-11-03 01:15:59 +0900

    [Feat] 웹소켓 서버

---
+++ WebSocketSV/typescript/websocket.ts
@@ -0,0 +1,43 @@
+import * as http from "http";
+import { WebSocketServer } from 'ws'
+import express from "express";
+
+class Main{
+
+    webexpress:express;
+    socketexpress:express;
+    websocket:WebSocketServer;
+
+    constructor(){
+
+        this.webexpress = express();
+        this.webexpress.use("/", express.static("./src/html"));
+        this.webexpress.use("/src", express.static("./src"));
+        this.webexpress.use("/typeout", express.static("./typeout"));
+
+        let webserver:http.Server = http.createServer(this.webexpress);
+        webserver.listen(8080);
+
+        let socketserver:http.Server = http.createServer(this.socketexpress);
+        socketserver.listen(8081);
+
+        this.websocket = new WebSocketServer({ server:socketserver });
+    }
+
+
+}
+
+
+let app:Main = new Main();
+
+app.websocket.on('connection', (ws)=>{
+    console.log("새 클라이언트 연결됨");
+
+    ws.on('message', (message)=>{
+        console.log("CLIENT SENT : " + message);
+    });
+
+    ws.on('close', ()=>{
+        console.log("client closed");
+    });
+});
